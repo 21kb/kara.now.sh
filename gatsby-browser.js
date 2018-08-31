47eca2332c75701f396bd49d67b0d46e0acbe5bb
Kunall Banerjee <yeskunall96@gmail.com>
2018-08-31 05:28:16 -0400

    Disable global styles
    Wait until we migrate to Gatsby v2

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,4 @@
 // This doesn't seem to be working. Is this a
 // Gatsby v2 thing only?
+// Sure looks like it from: https://next.gatsbyjs.org/docs/migrating-from-v1-to-v2/
 import './src/styles/global.css';
